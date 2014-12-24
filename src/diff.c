aac2b5cc8ac85d62cd10be868cd8b3e68c73e75a
Matthew Seaman <m.seaman@infracaninophile.co.uk>
2014-12-24 21:06:10 +0000

    Option parsing.

--- src/diff.c
+++ src/diff.c
@@ -24,6 +24,8 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <sysexits.h>
 
@@ -35,12 +37,64 @@
 void
 usage_diff(void)
 {
-	fprintf(stderr, "Usage: pkg diff [-qv] [-r repo] -a | -Cgix <pattern>\n\n");
+	fprintf(stderr, "Usage: pkg diff [-qv] [-r repo] -a | "
+	    "-Cgix <pattern>\n\n");
 	fprintf(stderr, "For more information see 'pkg help diff'.\n");
 }
 
 int
 exec_diff(int argc, char **argv)
 {
+	bool	verbose = false;
+	int	ch;
+	match_t match = MATCH_EXACT;
+
+	struct option longopts[] = {
+		{ "all",	      no_argument,       NULL, 'a' },
+		{ "case-sensitive",   no_argument,       NULL, 'C' },
+		{ "glob",	      no_argument,       NULL, 'g' },
+		{ "case-insensitive", no_argument,       NULL, 'i' },
+		{ "guiet",	      no_argument,       NULL, 'q' },
+		{ "repository",	      required_argument, NULL, 'r' },
+		{ "verbose",	      no_argument,       NULL, 'v' },
+		{ "regex",	      no_argument,       NULL, 'x' },
+		{ NULL,		      0,		 NULL, 0   },
+	};
+
+	while ((ch = getopt_long(argc, argv, "+aCgigr:vx", longopts, NULL))
+	       != -1) {
+		switch (ch) {
+		case 'a':
+			match = MATCH_ALL;
+			break;
+		case 'C':
+			pkgdb_set_case_sensitivity(true);
+			break;
+		case 'g':
+			match = MATCH_GLOB;
+			break;
+		case 'i':
+			pkgdb_set_case_sensitivity(false);
+			break;
+		case 'q':
+			quiet = true;
+			break;
+		case 'r':
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		case 'x':
+			match = MATCH_REGEX;
+			break;
+		default:
+			usage_diff();
+			return (EX_USAGE);
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	
 	return (EX_OK);
 }
